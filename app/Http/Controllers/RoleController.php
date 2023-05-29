2552dab2fa47a41ec19ea1c2b22e552ae8e06b59
Jello <jasmonasterial@gmail.com>
2023-05-29 10:37:49 +0800

    test simple code

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Role;
 
 class RoleController extends Controller
 {
     //
+
+    function add(Request $role){
+    $role = new Role;
+    $hello = 'Admin';
+    $role -> $hello = $role -> Name;   
+    
+    $role -> save();
+    return redirect('add'); 
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,5 @@ Route::get('/', function () {
     return view('welcome');
     //test
 });
+
+Route::post('add', [RoleController::class, 'data']);
