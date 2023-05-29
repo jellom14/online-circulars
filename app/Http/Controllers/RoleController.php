69beb632cb1e0ab6e0e92ad1a2d162046c95dec6
Jello <jasmonasterial@gmail.com>
2023-05-29 11:40:04 +0800

    more tests

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Http\Controllers\Controller;
 use App\Models\Role;
 
 class RoleController extends Controller
 {
     //
 
-    function add(Request $role){
-    $role = new Role;
-    $hello = 'Admin';
-    $role -> $hello = $role -> Name;   
-    
-    $role -> save();
-    return redirect('add'); 
+    public function createrole(){
+        return view('create_role');
+    }
+
+    public function insert(Request $role){
+    $name = $role -> input('name');   
+    $data = array('name'=>$name);
+    DB::table('roles')->insert($data);
+
+    echo "Record inserted successfully.<br/>";
 
 
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-   // return view('welcome');
-    //test
-//});
+/*
+Route::get('/', function () {
+    return view('create_role');
+}); 
+*/
 
-Route::get('add', [RoleController::class, 'data']);
-Route::post('add', [RoleController::class, 'data']);
+
+Route::get('insert','RoleController@createrole');
+Route::post('create','RoleController@insert');
